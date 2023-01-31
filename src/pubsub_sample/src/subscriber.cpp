cc330a70b40e1afd3a708c025c46b52cbae954d1
Tomohito Ando <tomohito.ando@tier4.jp>
2023-01-31 19:35:21 +0900

    first commit
    Signed-off-by: Tomohito Ando <tomohito.ando@tier4.jp>

---
+++ src/pubsub_sample/src/subscriber.cpp
@@ -0,0 +1,60 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <functional>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include <sample_msgs/msg/int32_stamped.hpp>
+
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+public:
+  MinimalSubscriber()
+      : Node("minimal_subscriber"), count_(0)
+  {
+    subscription_ = this->create_subscription<sample_msgs::msg::Int32Stamped>(
+        "topic1", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    publisher_ = this->create_publisher<sample_msgs::msg::Int32Stamped>("topic2", 10);
+  }
+
+private:
+  void topic_callback(const sample_msgs::msg::Int32Stamped &msg)
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg.data);
+
+    auto message = sample_msgs::msg::Int32Stamped();
+    message.data = msg.data * 2;
+    message.stamp = msg.stamp;
+
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.data);
+    publisher_->publish(message);
+  }
+
+  rclcpp::Subscription<sample_msgs::msg::Int32Stamped>::SharedPtr subscription_;
+  rclcpp::Publisher<sample_msgs::msg::Int32Stamped>::SharedPtr publisher_;
+  size_t count_;
+};
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/pubsub_sample/src/subscriber_sync.cpp
@@ -0,0 +1,79 @@
+#include <functional>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include <sample_msgs/msg/int32_stamped.hpp>
+
+// message filter
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/synchronizer.h>
+
+using sample_msgs::msg::Int32Stamped;
+using Int32StampedConstPtr = Int32Stamped::ConstSharedPtr;
+using std::placeholders::_1;
+namespace sync_policies = message_filters::sync_policies;
+
+class SyncedSubscriber : public rclcpp::Node
+{
+public:
+  using ExactTimeSyncPolicy = sync_policies::ExactTime<Int32Stamped, Int32Stamped>;
+  using ExactTimeSynchronizer = message_filters::Synchronizer<ExactTimeSyncPolicy>;
+
+  SyncedSubscriber()
+      : Node("minimal_subscriber")
+  {
+    // Subscribe the input using message filter
+    const size_t max_queue_size = 10;
+    sub_topic1_.subscribe(
+        this, "topic1", rclcpp::SensorDataQoS().keep_last(max_queue_size).get_rmw_qos_profile());
+    sub_topic2_.subscribe(
+        this, "topic2", rclcpp::SensorDataQoS().keep_last(max_queue_size).get_rmw_qos_profile());
+
+    // sync subscribers with Exact Time Sync Policy
+    // sync_inputs_ = std::make_shared<ExactTimeSynchronizer>(ExactTimeSyncPolicy(10), sub_topic1_, sub_topic2_);
+    sync_inputs_ = std::make_shared<ExactTimeSynchronizer>(max_queue_size);
+    sync_inputs_->connectInput(sub_topic1_, sub_topic2_);
+    sync_inputs_->registerCallback(&SyncedSubscriber::synced_topic_callback, this);
+
+    // This is NG
+    // sync_inputs_->registerCallback(std::bind(
+    //     &SyncedSubscriber::synced_topic_callback, this, std::placeholders::_1, std::placeholders::_2));
+
+    // This is OK
+    // sync_inputs_->registerCallback(std::bind(
+    //     &SyncedSubscriber::synced_topic_callback_shared_ptr, this, std::placeholders::_1, std::placeholders::_2));
+  }
+
+private:
+  void synced_topic_callback(const Int32Stamped &msg1, const Int32Stamped &msg2)
+  {
+    RCLCPP_INFO(this->get_logger(), "topic1: '%d'", msg1.data);
+    RCLCPP_INFO(this->get_logger(), "topic2: '%d'", msg2.data);
+    RCLCPP_INFO(this->get_logger(), "-----------");
+  }
+
+  void synced_topic_callback_shared_ptr(const Int32StampedConstPtr msg1, const Int32StampedConstPtr msg2)
+  {
+    RCLCPP_INFO(this->get_logger(), "topic1: '%d'", msg1->data);
+    RCLCPP_INFO(this->get_logger(), "topic2: '%d'", msg2->data);
+    RCLCPP_INFO(this->get_logger(), "-----------");
+  }
+
+  message_filters::Subscriber<Int32Stamped> sub_topic1_;
+  message_filters::Subscriber<Int32Stamped> sub_topic2_;
+
+  // Synchronized input
+  std::shared_ptr<ExactTimeSynchronizer> sync_inputs_;
+};
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SyncedSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
